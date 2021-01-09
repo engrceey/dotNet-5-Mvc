6693710a0e15e327282e468130b9eef5392114d5
engrceey <ogbondachristian@gmail.com>
2021-01-09 21:57:18 +0100

    add table to db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
+using BHRUGEN_MVC.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BHRUGEN_MVC.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
     }
 }
